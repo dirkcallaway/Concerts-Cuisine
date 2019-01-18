c172913427952e5acbffe140a559e1325232fdc5
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2019-01-17 20:49:18 -0700

    Added Zip form input and updated MapQuest API call

--- geocode.js
+++ geocode.js
@@ -1,13 +1,8 @@
 var mapQueryUrl = "http://www.mapquestapi.com/geocoding/v1/address?";
 var mapSearchObject = {
-    key : "h1AaSPSUGvuBlInfmGZQsZYqflUTxUri",
-    location : "80526"
+    key: "h1AaSPSUGvuBlInfmGZQsZYqflUTxUri",
+    location: ""
 };
-
-//Button Click eventually...
-mapQueryUrl += $.param(mapSearchObject);
-console.log(mapQueryUrl);
-
 
 var ajaxCall = function () {
     $.ajax({
@@ -20,4 +15,25 @@ var ajaxCall = function () {
         });
 };
 
-ajaxCall();
+//Submit Button Click eventually...
+$("#submit").on("click", function () {
+    //Grabs Value from input box
+    var zipCode = $("#zip").val().trim();
+    //Checks length of input... is it 5 long
+    if(zipCode.length > 5 || zipCode.length < 5){
+        //Alerts if more or less than 5 long
+        alert("Please enter a valid Zip Code.");
+    } else{
+        //Sets the location in the mapSearchObject
+        mapSearchObject.location = zipCode;
+    }
+    //Updates the mapQueryUrl
+    mapQueryUrl += $.param(mapSearchObject);
+    console.log(mapQueryUrl);
+    //Resets the zip input box
+    $("#zip").val("");
+    //Calls the MapQuest API
+    ajaxCall();
+});
+
+
