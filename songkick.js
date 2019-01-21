c498b6649645978a051eca2655d604b9ea830c50
Brit Armentrout <brittney.ann.armentrout@gmail.com>
2019-01-20 17:56:29 -0700

    retrieving concert data properly

--- songkick.js
+++ songkick.js
@@ -13,44 +13,79 @@
 
 //Songkick API variables
 var apiKey = "N3NwIQTcgTiC3jVE";
-var locationLatLong = "39.6303,-106.0434";
 var metroAreaId = "";
-var lat = 39.6903
-console.log(lat);
-var lon = -106.0434
-console.log(lon);
+var lat = 39.7392
+var lon = -104.9903
 
-
+var concertDetails = "";
+var concertCity = "";
+var concertLink = "";
 
 var concertQueryUrl = "https://api.songkick.com/api/3.0/search/locations.json?location=geo:" + lat + "," + lon + "&apikey=" + apiKey
 console.log(concertQueryUrl);
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
-$.ajax({
-        url: concertQueryUrl,
-        method: "GET"
-    })
-
-    .then(function (response) {
-        console.log(response);
-        
-        metroAreaId = response.resultsPage.results.location[0].metroArea.id;
-        console.log(metroAreaId);
-        
-        var concertQueryUrlbyId = "https://api.songkick.com/api/3.0/metro_areas/" + metroAreaId + "/calendar.json?&apikey=" + apiKey
-        console.log(concertQueryUrlbyId);
-
-        $.ajax({
-            url: concertQueryUrlbyId,
+    $.ajax({
+            url: concertQueryUrl,
             method: "GET"
         })
-    
+
         .then(function (response) {
             console.log(response);
+
+            metroAreaId = response.resultsPage.results.location[0].metroArea.id;
+            console.log("Metro Area ID: " + metroAreaId);
+
+            var concertQueryUrlbyId = "https://api.songkick.com/api/3.0/metro_areas/" + metroAreaId + "/calendar.json?&apikey=" + apiKey
+            console.log(concertQueryUrlbyId);
+
+            $.ajax({
+                    url: concertQueryUrlbyId,
+                    method: "GET"
+                })
+
+                .then(function (response) {
+                    console.log(response);
+
+                    for (var i = 0; i < 9; i++) {
+
+                        concertDetails = response.resultsPage.results.event[i].displayName;
+                        console.log("Details: " + concertDetails);
+
+                        concertCity = response.resultsPage.results.event[i].location.city;
+                        console.log("City, State: " + concertCity);
+
+                        concertLink = response.resultsPage.results.event[i].uri;
+                        console.log("Link: " + concertLink);
+
+                        //Adjust class name of concertCard to match color scheme
+                        var concertCard = $("<div class='card deep-purple lighten-1 concert-click'>");
+                        var cardContent = $("<div class='card-content white-text'>");
+
+                        var cardTitle = $("<span class='card-title'>").text("Event: " + concertDetails); //Link to SongKick Band Name
+
+                        var cardCity = $("<p>").text(concertCity); //Link to SongKick City, State, Country
+
+                        var cardLink = $("<p>").text("Tickets: " + concertLink); //Link to SongKick Website Link
+
+
+                        //Puts the card parts together
+                        cardContent.append(cardTitle);
+                        cardContent.append(cardCity);
+                        cardContent.append(cardLink);
+
+                        //Pushes finished card into the HTML
+                        $("#concerts").append(concertCard);
+                        $("#concerts").addClass("focus");
+                        //Need to remove old search when new zip submitted 
+
+
+                    }
+                });
+
+
         });
-    });
 
-    });
-
+});
