53384b9ae178c969395d75c9664abba106cd8119
Dirk Callaway <35826733+dirkcallaway@users.noreply.github.com>
2019-01-21 17:23:10 -0700

    Integrated SongKick into live page.

--- songkick.js
+++ songkick.js
@@ -24,72 +24,72 @@ var concertLink = "";
 
 $(document).ready(function () {
 
-    var populateConcerts = function() {
-    
+    var populateConcerts = function () {
+
         var concertQueryUrl = "https://api.songkick.com/api/3.0/search/locations.json?location=geo:" + lat + "," + lon + "&apikey=" + apiKey
         console.log(concertQueryUrl);
-    
+
         $.ajax({
-            url: concertQueryUrl,
-            method: "GET"
-        })
+                url: concertQueryUrl,
+                method: "GET"
+            })
 
-        .then(function (response) {
-            console.log(response);
+            .then(function (response) {
+                console.log(response);
 
-            metroAreaId = response.resultsPage.results.location[0].metroArea.id;
-            console.log("Metro Area ID: " + metroAreaId);
+                metroAreaId = response.resultsPage.results.location[0].metroArea.id;
+                console.log("Metro Area ID: " + metroAreaId);
 
-            var concertQueryUrlbyId = "https://api.songkick.com/api/3.0/metro_areas/" + metroAreaId + "/calendar.json?&apikey=" + apiKey
-            console.log(concertQueryUrlbyId);
+                var concertQueryUrlbyId = "https://api.songkick.com/api/3.0/metro_areas/" + metroAreaId + "/calendar.json?&apikey=" + apiKey
+                console.log(concertQueryUrlbyId);
 
-            $.ajax({
-                    url: concertQueryUrlbyId,
-                    method: "GET"
-                })
+                $.ajax({
+                        url: concertQueryUrlbyId,
+                        method: "GET"
+                    })
 
-                .then(function (response) {
-                    console.log(response);
+                    .then(function (response) {
+                        console.log(response);
 
-                    for (var i = 0; i < 9; i++) {
+                        for (var i = 0; i < 9; i++) {
 
-                        concertDetails = response.resultsPage.results.event[i].displayName;
-                        console.log("Details: " + concertDetails);
+                            concertDetails = response.resultsPage.results.event[i].displayName;
+                            console.log("Details: " + concertDetails);
 
-                        concertCity = response.resultsPage.results.event[i].location.city;
-                        console.log("City, State: " + concertCity);
+                            concertCity = response.resultsPage.results.event[i].location.city;
+                            console.log("City, State: " + concertCity);
 
-                        concertLink = response.resultsPage.results.event[i].uri;
-                        console.log("Link: " + concertLink);
+                            concertLink = response.resultsPage.results.event[i].uri;
+                            console.log("Link: " + concertLink);
 
-                        //Adjust class name of concertCard to match color scheme
-                        var concertCard = $("<div class='card deep-purple lighten-1 concert-click'>");
-                        var cardContent = $("<div class='card-content white-text'>");
+                            //Adjust class name of concertCard to match color scheme
+                            var concertCard = $("<div class='card deep-purple lighten-1 concert-click'>");
+                            var cardContent = $("<div class='card-content white-text'>");
 
-                        var cardTitle = $("<span class='card-title'>").text("Event: " + concertDetails); //Link to SongKick Band Name
+                            var cardTitle = $("<span class='card-title'>").text("Event: " + concertDetails); //Link to SongKick Band Name
 
-                        var cardCity = $("<p>").text(concertCity); //Link to SongKick City, State, Country
+                            var cardCity = $("<p>").text(concertCity); //Link to SongKick City, State, Country
 
-                        var cardLink = $("<p>").text("Tickets: " + concertLink); //Link to SongKick Website Link
-
-
-                        //Puts the card parts together
-                        cardContent.append(cardTitle);
-                        cardContent.append(cardCity);
-                        cardContent.append(cardLink);
-
-                        //Pushes finished card into the HTML
-                        $("#concerts").append(concertCard);
-                        $("#concerts").addClass("focus");
-                        $("#concerts").addClass("active");
-                        //Need to remove old search when new zip submitted 
+                            var cardLink = $("<p>").text("Tickets: " + concertLink); //Link to SongKick Website Link
 
 
-                    }
-                });
+                            //Puts the card parts together
+                            cardContent.append(cardTitle);
+                            cardContent.append(cardCity);
+                            cardContent.append(cardLink);
+
+                            //Pushes finished card into the HTML
+                            $("#concerts").append(concertCard);
+                            $("#concerts").addClass("focus");
+                            $("#concerts").addClass("active");
+                            //Need to remove old search when new zip submitted 
 
 
-        });
+                        }
+                    });
 
-};
+
+            });
+
+    };
 });
